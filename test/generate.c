e6ab34baadf014a58b8ab70ce934991f4584102f
Neil <neil@sdf.org>
2025-02-03 06:01:06 -0800

    endianness

--- test/generate.c
+++ test/generate.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
-#include <stdint.h>
+#include <assert.h>
+//#include <stdint.h>
+#include <inttypes.h>
 #include <stdbool.h>
 
 /* X-macro from
@@ -11,18 +13,19 @@
 	X(Nd), X(Nl), X(No), X(Pc), X(Pd), X(Pe), X(Pf), X(Pi), X(Po), X(Ps), \
 	X(Sc), X(Sk), X(Sm), X(So), X(Zl), X(Zp), X(Zs)
 
-/*#define X(n) #n
+#define X(n) #n
 static const char *category_strings[] = { CATEGORIES };
-#undef X*/
+#undef X
 
 /** Note. Utf-8 disallows overlong encodings. Should be "modified utf-8" which
  uses one overlong encoding of U+0000 as "0xC0, 0x80" to solve it conflicting
  with '\0' as nul-termination. We just won't care about that. */
 struct unicode {
+	uint32_t unicode;
 #define X(n) n
 	enum character_category { CATEGORIES } category;
 #undef X
-	char utf8codepoint[5];
+	char utf8[5]; /* In utf-8. */
 	uint8_t bytes;
 	uint8_t is_word;
 };
@@ -30,21 +33,31 @@ struct unicode {
 /* Visualization trie. The Patricia tree has don't-care bits, so is not
  applicable to inverse-range-queries. (Nice try, tough.) */
 static const char *unicode_key(const struct unicode *const*const u)
-	{ return (*u)->utf8codepoint; }
+	{ return (*u)->utf8; }
 #define TRIE_NAME unicode
 #define TRIE_ENTRY struct unicode *
 #define TRIE_TO_STRING
 #include "../src/trie.h"
 
-static void unicode_to_string(const struct unicode *const*const u, char (*const a)[12]) {
-	const char *const uni = unicode_key(u);
-	strcpy(*a, unicode_key(u));
-}
+static void unicode_to_string(const struct unicode *const u, char (*const a)[12])
+	{ sprintf(*a, "U+%x", u->unicode % 0x1000000); }
 #define DEQUE_NAME unicode
 #define DEQUE_TYPE struct unicode
 #define DEQUE_FRONT
 #define DEQUE_TO_STRING
 #include "../src/deque.h"
+
+static void print_unicode(const struct unicode *const u) {
+	char a[12];
+	unicode_to_string(u, &a);
+	fprintf(stderr, "%s: (%"PRIu8")", a, u->bytes);
+	assert(u->bytes < 4);
+	for(unsigned i = 0; i < u->bytes; i++) {
+		fprintf(stderr, " %.2x", u->utf8[i]);
+	}
+	fprintf(stderr, " \"%s\" %s%s.\n", u->utf8, category_strings[u->category],
+		u->is_word ? " belongs to a word" : "");
+}
 
 int main(void) {
 	const char *errmsg = 0;
@@ -52,10 +65,8 @@ int main(void) {
 	FILE *unicode_fp = 0;
 	char read[256];
 	struct unicode_deque storage = unicode_deque();
-	struct unicode_trie bytetrie[4] = { {0}, {0}, {0}, {0} };
 	struct unicode *u = 0;
 	errno = 0;
-	bool first = true;
 
 	/* Load the unicode data into `deque`.
 	 <https://www.unicode.org/Public/UNIDATA/UnicodeData.txt> */
@@ -66,8 +77,36 @@ int main(void) {
 		struct { unsigned unicode; char category[4]; } input;
 		if(sscanf(read, "%x;%*[^;];%3[^;]", &input.unicode,
 			input.category) != 2) { errmsg = unicode_fn; goto catch; };
-		if(input.unicode >= 0x80) continue; /* Debug ascii. Much smaller set. */
 		if(!(u = unicode_deque_new_back(&storage))) goto catch;
+		u->unicode = input.unicode;
+		if(input.unicode < 0x80) {
+			u->bytes = 1;
+			u->utf8[0] = (char)input.unicode;
+			u->utf8[1] = '\0';
+		} else if(input.unicode < 0x0800) {
+			u->bytes = 2;
+			u->utf8[0] = 0xc0 | (char) (input.unicode >>  6u);
+			u->utf8[1] = 0x80 | (char)( input.unicode         & 0x3f);
+			u->utf8[2] = '\0';
+		} else if(input.unicode < 0x010000) {
+			u->bytes = 3;
+			u->utf8[0] = 0xe0 | (char)(input.unicode  >> 12u);
+			u->utf8[1] = 0x80 | (char)((input.unicode >>  6u) & 0x3f);
+			u->utf8[2] = 0x80 | (char)( input.unicode         & 0x3f);
+			u->utf8[3] = '\0';
+		} else if(input.unicode < 0x110000) {
+			u->bytes = 4;
+			u->utf8[0] = 0xf0 | (char) (input.unicode >> 18u);
+			u->utf8[1] = 0x80 | (char)((input.unicode >> 12u) & 0x3f);
+			u->utf8[2] = 0x80 | (char)((input.unicode >>  6u) & 0x3f);
+			u->utf8[3] = 0x80 | (char)( input.unicode         & 0x3f);
+			u->utf8[4] = '\0';
+		} else {
+			fprintf(stderr, "Only supports 0x110_000 code points in unicode 16.0.0 #44.\n");
+			errno = ERANGE;
+			errmsg = unicode_fn;
+			goto catch;
+		}
 		enum character_category cc = WTF;
 		switch(input.category[0]) {
 		case 'C':
@@ -137,50 +176,26 @@ int main(void) {
 		}
 		u->category = cc;
 		u->is_word = (cc == Ll || cc == Lu || cc == Lt || cc == Lo || cc == Nd);
-		if(input.unicode < 0x80) {
-			u->utf8codepoint[0] = (char)input.unicode;
-			u->utf8codepoint[1] = '\0';
-		} else if(input.unicode < 0x0800) {
-			u->utf8codepoint[0] = 0xc0 | (char) (input.unicode >>  6u);
-			u->utf8codepoint[1] = 0x80 | (char)( input.unicode         & 0x3f);
-			u->utf8codepoint[2] = '\0';
-		} else if(input.unicode < 0x010000) {
-			u->utf8codepoint[0] = 0xe0 | (char)(input.unicode  >> 12u);
-			u->utf8codepoint[1] = 0x80 | (char)((input.unicode >>  6u) & 0x3f);
-			u->utf8codepoint[2] = 0x80 | (char)( input.unicode         & 0x3f);
-			u->utf8codepoint[3] = '\0';
-		} else if(input.unicode < 0x110000) {
-			u->utf8codepoint[0] = 0xf0 | (char) (input.unicode >> 18u);
-			u->utf8codepoint[1] = 0x80 | (char)((input.unicode >> 12u) & 0x3f);
-			u->utf8codepoint[2] = 0x80 | (char)((input.unicode >>  6u) & 0x3f);
-			u->utf8codepoint[3] = 0x80 | (char)( input.unicode         & 0x3f);
-			u->utf8codepoint[4] = '\0';
-		} else {
-			fprintf(stderr, "Only supports 0x110_000 code points in unicode 16.0.0.\n");
-			errno = ERANGE;
-			errmsg = unicode_fn;
-			goto catch;
-		}
-		if(first) {
-			//unicode_deque_graph_fn(&storage, "first.gv");
-			// I can't figure that out.
-			first = false;
-		}
 	}
 	fclose(unicode_fp), unicode_fp = 0;
-	//unicode_deque_graph_fn(&storage, "storage.gv");
+	unicode_deque_graph_fn(&storage, "storage.gv");
 
 	/* Output C. */
+	struct unicode_trie bytetrie[4] = { {0}, {0}, {0}, {0} };
 	bool is_in_word = false, is_first = true;
 	struct { size_t nots, words; } count = { 0, 0 };
+	unsigned bytes_prev = 0;
 	for(struct unicode_deque_cursor cur = unicode_deque_begin(&storage);
 		unicode_deque_exists(&cur); unicode_deque_next(&cur)) {
 		struct unicode **put_data_here;
 		u = unicode_deque_entry(&cur);
-		/* Put both entries in the trie if it's a rising-or-falling-edge. */
+		/* We rely on sorted date so that we can not come back to the same
+		 byte-count. Otherwise each trie would have it's own. */
+		if(u->bytes != bytes_prev) { is_in_word = false; bytes_prev = u->bytes; }
+		/* Put it in trie if it's a rising-or-falling-edge. */
 		if(!(is_in_word ^ u->is_word)) continue;
 		is_in_word = u->is_word;
-		switch(unicode_trie_add(&bytetrie[u->bytes], u->utf8codepoint, &put_data_here)) {
+		switch(unicode_trie_add(&bytetrie[u->bytes - 1], u->utf8, &put_data_here)) {
 		case TRIE_PRESENT:
 			fprintf(stderr, "Has duplicate code-points.\n");
 			errno = EDOM;
@@ -188,46 +203,59 @@ int main(void) {
 		case TRIE_ABSENT:
 			*put_data_here = u;
 			if(u->is_word) count.words++; else count.nots++;
-			printf("%s\"%s\"", is_first ? "" : ", ", u->utf8codepoint);
+			fprintf(stderr, "%s\"%s\"", is_first ? "" : ", ", u->utf8);
 			is_first = false;
 			break;
 		}
 	}
 	printf(".\n");
-	unicode_trie_graph_all(&bytetrie[0], "trie0.gv", 0);
 
-#if 0
+	/* Detect the endianness. */
+	const union { uint32_t a; uint8_t b[4]; } test = { .a = 1 };
+	fprintf(stderr, "(%"PRIu32") == (%"PRIu8",%"PRIu8",%"PRIu8",%"PRIu8")\n",
+		test.a, test.b[0], test.b[1], test.b[2], test.b[3]);
+	assert((test.b[0] == 1) ^ (test.b[3] == 1) && !test.b[1] && !test.b[2]);
+	bool right_to_left = (test.b[0] ? true : false);
+	fprintf(stderr, "It's %s.\n",
+		right_to_left ? "right-to-left" : "left-to-right");
 
-#define ONLY_2_BYTES
-#ifdef ONLY_1_BYTE
-		if(input.unicode >= 0x80) continue;
-#elif defined ONLY_2_BYTES
-		if(input.unicode < 0x80 || input.unicode >= 0x0800) continue;
-#elif defined ONLY_3_BYTES
-		if(input.unicode < 0x8000 || input.unicode >= 0x010000) continue;
-#elif defined ONLY_4_BYTES
-		if(input.unicode < 0x010000 || input.unicode >= 0x110000) continue;
-#endif
-
-
-
+	/* Output the programme. */
+	for(unsigned i = 0; i < 4; i++) {
+		char trie_name[32];
+		bool first = true;
+		sprintf(trie_name, "trie-%u-byte.gv", i + 1);
+		unicode_trie_graph_all(&bytetrie[i], trie_name, 0);
+		printf("static char uniedge[%u] = {\n"
+			"\t", i);
+		for(struct unicode_trie_cursor cur = unicode_trie_begin(&bytetrie[i]);
+			unicode_trie_exists(&cur); unicode_trie_next(&cur)) {
+			u = *unicode_trie_entry(&cur);
+			uint32_t utf8code = 0; /* fixme: endianness. */
+			bool first_inner = true;
+			for(unsigned byte = 0; byte < u->bytes; byte++) {
+				printf("%s(0x%"PRIx8")", first_inner ? "" : ":", (uint8_t)u->utf8[byte]);
+				first_inner = false;
+			}
+			for(unsigned byte = 0; byte < u->bytes; byte++) {
+				if(right_to_left)
+					utf8code |= (uint32_t)(uint8_t)u->utf8[byte] << (byte * 8u);
+				else
+					utf8code = (utf8code << 8u) | (uint8_t)u->utf8[byte];
+			}
+			printf("->"/*"%s"*/"0x%"PRIx32"\"%s\"\n", /*first ? "" : ", ",*/ utf8code, u->utf8);
+			first = false;
+		}
+		printf("\n"
+			"};\n");
 	}
-	if(ferror(uni_fp)) goto catch; /* May not produce a meaningful error. */
-	mutf8_trie_graph_all(&trie, "graph.gv", 0);
 
-	/*char *test = "a";
-	mutf8 = mutf8_trie_get(&trie, test);
-	fprintf(stderr, "\"%s\" is %s and %s in the set of word-code-points.\n",
-		test, category_strings[mutf8->category],
-		mutf8->word ? "belongs" : "does not belong");*/
 	fprintf(stderr, "There are %zu words and %zu not-words. Total %zu.\n", count.words, count.nots, count.words + count.nots);
-#endif
 
 	goto finally;
 catch:
 	perror(errmsg);
 finally:
-	unicode_trie_(&bytetrie);
+	for(size_t i = 0; i < 4; i++) unicode_trie_(&bytetrie[i]);
 	unicode_deque_(&storage);
 	if(unicode_fp) fclose(unicode_fp), unicode_fp = 0;
 	return errmsg ? EXIT_FAILURE : EXIT_SUCCESS;
