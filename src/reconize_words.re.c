3df9c675c48c6ae5ffa1e5fb356e652d1e054f4a
Neil <neil@sdf.org>
2025-02-08 11:32:14 -0800

    Got it.

--- src/reconize_words.re.c
+++ src/reconize_words.re.c
@@ -7,7 +7,7 @@ struct word { union { const char *c; const uint8_t *u; } start, end; };
 /*!include:re2c "unicode_categories.re" */
 
 /** To begin, `w` end must be set to the string. */
-static void next_word(struct subword *const w) {
+static void next_word(struct word *const w) {
 	const uint8_t *YYCURSOR = w->end.u, *YYMARKER, *yyt1 = 0, *w0, *w1;
 	for( ; ; ) {
 		/*!re2c
@@ -19,21 +19,27 @@ static void next_word(struct subword *const w) {
 		// (see https://unicode.org/reports/tr31) (modified)
 		// Letter and any marks that modify it.
 		// <https://www.regular-expressions.info/unicode.html>
-		letter = L M*;
-		//word_end = letter | N;
-		//word_mid = word_end | Pc | ['\u200b\u200c\u200d\u05F3];
-		//word     = word_end (word_mid* word_end)?;
-		//binary_digit = [01];
-		//hex_digit = [0-9a-fA-F];
-		//octal_digit = [0-7];
-		//separator = [_]; // Python syntax.
-		//decimal = [.];
-		//binary = "b" binary_digit+ (separator binary_digit+)*;
-		//hex = "0x" hex_digit+ (separator hex_digit+)*;
-		//integer = N+ (separator N+)*;
-		//money = integer (decimal N N)? Sc;
-		//word_like = word | binary | hex | integer | money;
-		word_like = letter+;
+		binary_digit = [01];
+		hex_digit = [0-9a-fA-F];
+		octal_digit = [0-7];
+		dec_digit = [0-9];
+		all_other_numeric = N \ [0-9]; // Number.
+		separator = [_]; // Python syntax.
+		decimal = [.];
+		minus = [-];
+		exp = [e];
+		binary = "b" minus? binary_digit+ (separator binary_digit+)*;
+		hex = "0x" minus? hex_digit+ (separator hex_digit+)*;
+		integer = minus? dec_digit+ (separator dec_digit+)*;
+		money = minus? integer (decimal dec_digit{2,2})? Sc; // Symbol currency. Probably not inclusive
+		real = minus? integer decimal integer (exp minus? integer (decimal integer)?)?;
+		number = binary | hex | integer | real | money;
+		letter = L M*; // Letter modifier.
+		word_begin = letter | all_other_numeric;
+		word_mid = word_begin | Pc | [\u200b\u200c\u200d\u05F3]; // Connector punctuation. We specifically don't do "\'" because they are probably separate words.
+		word_end = word_begin;
+		word     = word_begin (word_mid* word_end)?;
+		word_like = (word | number)+;
 
 		"\x00" { w->start.u = 0; return; }
 		@w0 word_like @w1 { w->start.u = w0; w->end.u = w1; return; }
@@ -47,7 +53,7 @@ static void next_word(struct subword *const w) {
 #define TEST(str, num) \
 s = str; \
 for(sub.end.c = s, count = 0; ; ) { \
-	next_subword(&sub); \
+	next_word(&sub); \
 	if(!sub.start.c) break; \
 	printf("\"%.*s\"\n", (int)(sub.end.c - sub.start.c), sub.start.c); \
 	count++; \
