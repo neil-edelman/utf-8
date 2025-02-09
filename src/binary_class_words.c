5ec5439b2f9eb118db6f7e76236f1175d378c0e7
Neil <neil@sdf.org>
2025-02-08 20:51:43 -0800

    Made it more like the rest for test.

--- src/binary_class_words.c
+++ src/binary_class_words.c
@@ -190,47 +190,64 @@ static bool is_word(const char *const string_in_utf8,
 }
 /* --> */
 
-//struct word { union { const char *c; const uint8_t *u; } start, end; };
+struct word { union { const char *c; const uint8_t *u; } start, end; };
 
 #include <stdlib.h>
 #include <stdio.h>
 
-static void split(const char *const split) {
-	struct {
-		bool last, word;
-		const char *lastword;
-	} state = {0};
-	const char *s = split;
-	printf("->%s\n", s);
-	while(*s != '\0') {
-		const char *const lastword = s;
-		state.word = is_word(s, &s);
-		if(!state.word ^ state.last) continue;
-		state.last = state.word;
-		if(state.word) state.lastword = lastword;
-		else printf("\"%.*s\"\n", (int)(s - 1 - state.lastword), state.lastword);
+static void next_word(struct word *const w) {
+	const char *cursor = w->end.c, *next;
+	while(*cursor != '\0') {
+		if(!is_word(cursor, &next)) { cursor = next; continue; }
+		w->start.c = cursor, cursor = next;
+		while(is_word(cursor, &next)) cursor = next;
+		w->end.c = cursor;
+		return;
 	}
-	if(state.word) printf("\"%.*s\"\n", (int)(s - state.lastword), state.lastword);
+	w->start.c = 0;
 }
 
+#include <assert.h>
+
 int main(void) {
-	const char *sentence[] = {
-		"one two", //2
-		"one,two", //2
-		"one.two", //2
-		"one_two", //1 (1)
-		"one—two", //2
-		"0.0 a.b", //4
-		"fi f‌iii", //2 (U+200C zwnj is in the second)
-		"manœuver…æroplane non-joinerfif‌‌itt‌‌tt what?", // 4 zwnj
-		"100,000_000$", // 3 (2)
-		"zzzz̀abc de f̀ghi", // 3 (z, U+0300…f, U+0300 combining grave accent)
-		"나는  Chicago에  산다" // 3 (actually 5?)
+	const struct test { const char *sentence; size_t wc; } sentences[] = {
+		{ "", 0 },
+		{ " ,...,,,,.,., ~~~`````", 0 },
+		{ " yo", 1 },
+		{ "one two", 2 },
+		{ "one,two", 2 },
+		{ "one.two", 2 },
+		{ "one_two", 1 },
+		{ "one—two", 2 },
+		{ "(1-1i)", 2 },
+		{ "0.0 a.b", 4 },
+		{ "fi f‌iii", 2 }, /* U+200C zwnj is in the second. */
+		{ "manœuver…æroplane non-joinerfif‌‌itt‌‌tt good?", 5 }, /* 4 zwnj */
+		{ "100,000_000$", 2 },
+		{ "zzzz̀abc de f̀ghi", 3 }, /* z U+0300…f U+0300 combining grave accent. */
+		{ "나는  Chicago에  산다", 3 }, /* Actually 5? */
+		{ "_Ыдентификатор", 1 }, /* From re2c. "_" word. */
+		{ "\x80\x80""a a", 2 }, /* Proper handling of non-utf-8. */
+		{ "\xc0\x00""a a a a a a", 0 },
+		{ "\xe0\x00""a a a a a a", 0 },
+		{ "\xf0\x00""a a a a a a", 0 },
+		{ "\xf8\x00""a a a a a a", 0 },
+		{ "\xf8""a a a a a a", 6 }
 	};
-	for(const char **s = sentence,
-		**s_end = s + sizeof sentence / sizeof *sentence;
-		s < s_end; s++)
-		split(*s), printf("\n");
+	struct word word;
+	for(const struct test *t = sentences,
+		*t_end = t + sizeof sentences / sizeof *sentences;
+		t < t_end; t++) {
+		size_t count = 0;
+		for(word.end.c = t->sentence; ; ) {
+			next_word(&word);
+			if(!word.start.c) break;
+			printf("\"%.*s\"\n", (int)(word.end.c - word.start.c), word.start.c);
+			count++;
+		}
+		printf("\"%s\": %zu; supposed to be %zu.\n", t->sentence, count, t->wc);
+		assert(count == t->wc);
+	}
 
 	return EXIT_SUCCESS;
 }
